e46e608e3d4fdeaea6cb90bb2408a54b26f057ab
Jakub Czajka <CzajkaPL@o2.pl>
2021-10-29 09:53:19 +0200

    inicial commit

---
+++ cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Crypto_Exchange.dir/main.cpp.obj"
+  "Crypto_Exchange.exe"
+  "Crypto_Exchange.exe.manifest"
+  "Crypto_Exchange.pdb"
+  "libCrypto_Exchange.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Crypto_Exchange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
