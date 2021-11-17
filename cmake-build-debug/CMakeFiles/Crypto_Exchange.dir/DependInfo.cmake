a37ca3efd4f6088cce70fd7d46cd74c0fde2dc81
Jakub Czajka <CzajkaPL@o2.pl>
2021-11-17 19:58:08 +0100

    x

---
+++ cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/programowanie/GitHub/c++/Crypto_Exchange/classes/Admin.cpp" "D:/programowanie/GitHub/c++/Crypto_Exchange/cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/classes/Admin.cpp.obj"
+  "D:/programowanie/GitHub/c++/Crypto_Exchange/classes/Currency.cpp" "D:/programowanie/GitHub/c++/Crypto_Exchange/cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/classes/Currency.cpp.obj"
+  "D:/programowanie/GitHub/c++/Crypto_Exchange/classes/ExchangeDatabase.cpp" "D:/programowanie/GitHub/c++/Crypto_Exchange/cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/classes/ExchangeDatabase.cpp.obj"
+  "D:/programowanie/GitHub/c++/Crypto_Exchange/classes/User.cpp" "D:/programowanie/GitHub/c++/Crypto_Exchange/cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/classes/User.cpp.obj"
+  "D:/programowanie/GitHub/c++/Crypto_Exchange/main.cpp" "D:/programowanie/GitHub/c++/Crypto_Exchange/cmake-build-debug/CMakeFiles/Crypto_Exchange.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
